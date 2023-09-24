3a93b9cfdff0cc342c47e2ff28f0e16229d8aada
Chris Gambrell <cgambrell@orchardsoft.com>
2023-09-23 23:26:29 -0400

    modify field modification api to allow new field / subfield combinations to be created if they do not already exist.

--- src/hl7/hl7.rs
+++ src/hl7/hl7.rs
@@ -6,25 +6,37 @@ pub struct HL7_Segment {
 
 impl HL7_Segment {
     /// Allows modification of a given field/subfield combinations within the HL7 Segment.
-    pub fn modify_field(&mut self, field_position: usize, sub_field_position: usize, new_data: String) {
+    pub fn modify_field(&mut self, field_position: usize, sub_field_position: usize, new_data: String) -> &mut HL7_Segment {
         if sub_field_position - 1 < 0 {
             println!("Cannot modify a subfield that is lower than 1.");
-            return
+            return self;
         }
 
         if field_position - 1 < 0 {
             println!("Cannot modify a field that is lower than 1.");
-            return
+            return self;
         }
 
-        let mut field_to_modify: &mut HL7_Field = self.contents.get_mut(field_position - 1).expect("Field expected to exist.");
-        let mut subfield_to_modify = field_to_modify.sub_fields.get_mut(sub_field_position - 1);
-        if let Some(to_modify) = subfield_to_modify {
-            to_modify.data = new_data;
+        let mut field_to_modify = self.contents.get_mut(field_position - 1);
+        if let Some(field_to_modify) = field_to_modify {
+            let mut subfield_to_modify = field_to_modify.sub_fields.get_mut(sub_field_position - 1);
+            if let Some(subfield_to_modify) = subfield_to_modify {
+                subfield_to_modify.data = new_data;
+            }
+            else {
+                field_to_modify.sub_fields.push(HL7_Subfield { position: sub_field_position.to_string(), data: new_data.to_string() })
+            }
         }
         else {
-            field_to_modify.sub_fields.push(HL7_Subfield { position: sub_field_position.to_string(), data: new_data.to_string() })
+            let mut new_field = HL7_Field {
+                position: field_position.to_string(),
+                data: new_data.to_string(),
+                sub_fields: vec![],
+            };
+            new_field.sub_fields.push(HL7_Subfield { position: sub_field_position.to_string(), data: new_data.to_string() });
         }
+
+        self
     }
 }
 
